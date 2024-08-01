50180f90a65fb47a8c97bbc0bbce767f8298b4c4
PierreBarge <pierrebarge@hotmail.fr>
2024-08-01 21:05:47 +0200

    Feature: React app still going on

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,10 +2,44 @@ import PropTypes from "prop-types";
 import "./Modal.css";
 
 export default function Modal({ setButtonPressed }) {
+  const post = () => {
+    fetch("http://localhost:3000/author", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstname: "",
+        lastname: "",
+        birthdate: "",
+      }),
+    });
+  };
+
   return (
     <div className="modal">
-      <p>Bongour</p>
-      <button onClick={() => setButtonPressed(() => "None")}>Cancel</button>
+      <h2>Please fill this form to add an author</h2>
+      <form>
+        <div>
+          <label htmlFor="firstname">Author&apos;s firstname: </label>
+          <input type="text" name="firstname" id="firstname" required />
+        </div>
+        <div>
+          <label htmlFor="lastname">Author&apos;s lastname: </label>
+          <input type="text" name="lastname" id="lastname" required />
+        </div>
+        <div>
+          <label htmlFor="birthdate">Author&apos;s birthdate: </label>
+          <input type="date" name="birthdate" id="birthdate" />
+        </div>
+        <input type="submit" value="Submit" onSubmit={post()} />
+      </form>
+      <button
+        className="cancel-button"
+        onClick={() => setButtonPressed(() => "None")}>
+        Cancel
+      </button>
     </div>
   );
 }
